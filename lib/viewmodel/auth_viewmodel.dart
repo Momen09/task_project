1a107ceae809f7f8b6b3d861fc942f46ae1587d5
Mahmoud Said <dadosaid35@gmail.com>
2024-06-09 12:02:48 +0300

    .

--- lib/viewmodel/auth_viewmodel.dart
+++ lib/viewmodel/auth_viewmodel.dart
@@ -1,34 +1,32 @@
 import 'package:flutter/foundation.dart';
+import 'package:dio/dio.dart';
 import 'package:task1/consts/consts.dart';
-import 'package:task1/services/auth_service.dart';
-import '../model/auth_model.dart';
 
 class AuthViewModel extends ChangeNotifier {
   bool _isLoggedIn = false;
   bool _isLoading = false;
-  String? _accessToken;
-  String? _refreshToken;
+  String? _token;
 
   bool get isLoggedIn => _isLoggedIn;
   bool get isLoading => _isLoading;
-  String? get accessToken => _accessToken;
-  String? get refreshToken => _refreshToken;
-
-  final AuthService _authService = AuthService();
 
   Future<void> login(String username, String password) async {
     _isLoading = true;
     notifyListeners();
 
     try {
-      final authResponse = await _authService.login(username, password);
-      _accessToken = authResponse.accessToken;
-      _refreshToken = authResponse.refreshToken;
-      _isLoggedIn = true;
-      notifyListeners();
+      var response = await Dio().post(authUrl, data: {
+        'username': username,
+        'password': password,
+      });
+
+      if (response.statusCode == 200) {
+        _token = response.data['token'];
+        _isLoggedIn = true;
+        notifyListeners();
+      }
     } catch (e) {
-      // Handle login failure
-      print('Error in login: $e');
+
     } finally {
       _isLoading = false;
       notifyListeners();
@@ -37,8 +35,7 @@ class AuthViewModel extends ChangeNotifier {
 
   void logout() {
     _isLoggedIn = false;
-    _accessToken = null;
-    _refreshToken = null;
+    _token = null;
     notifyListeners();
   }
 }
